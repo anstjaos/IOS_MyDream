c00a3e92f39583c44843a83a7e02253398cda748
moonseo.kim <moonseo.kim@nhn.com>
2021-04-14 19:12:42 +0900

    Init project

---
+++ MyDream/Shared/MyDreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDreamApp.swift
+//  Shared
+//
+//  Created by NHN on 2021/04/14.
+//
+
+import SwiftUI
+
+@main
+struct MyDreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
