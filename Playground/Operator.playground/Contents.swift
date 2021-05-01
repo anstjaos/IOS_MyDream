f35c22c39b3ae371071d7fb975e3de358553c2b7
moonseo.kim <moonseo.kim@nhn.com>
2021-05-01 16:05:15 +0900

    practice operator

---
+++ Playground/Operator.playground/Contents.swift
@@ -0,0 +1,22 @@
+// 산술 연산자
+var weight = 65
+weight += 10
+weight -= 10
+weight *= 10
+weight /= 10
+weight %= 10
+
+
+// 비교 연산자
+// <, >, ==, !=
+let age = 14
+let isAdult = age >= 20
+let 중1 = age == 14
+
+// 논리 연산자
+// &&, ||, !
+// 조건식1 && 조건식2 => 두 가지 조건식 모두 true일 때 결과식 true
+let 초등학생 = age >= 8 && age <= 13
+
+// 조건식1 || 조건식2 => 두 가지 조건식 중 하나 이상 true일 때 결과 true
+
