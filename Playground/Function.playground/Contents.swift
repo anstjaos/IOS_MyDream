c49d6034fef90eedb326bccf511aedabd8e5ca64
moonseo.kim <moonseo.kim@nhn.com>
2021-05-02 16:58:29 +0900

    practice variable function

--- Playground/Function.playground/Contents.swift
+++ Playground/Function.playground/Contents.swift
@@ -7,3 +7,23 @@ func hello(name: String) -> String {
 let mesage = hello(name: "asdf")
 print("Hi~~")
 print(mesage)
+
+
+func addTwoNumbers(num1: Int, num2: Int = 100) -> Int {
+    return num1 + num2
+}
+
+addTwoNumbers(num1: 5, num2: 10)
+addTwoNumbers(num1: 5)
+
+func addNumbers(numbers: Int...) -> Int {
+    var sum = 0;
+    for num in numbers{
+        sum += num
+    }
+    return sum
+}
+
+addNumbers(numbers: 1, 2, 3, 4, 5)
+
+
