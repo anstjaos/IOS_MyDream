c410ed0d583dbafe4a29a4da9f9582f2747067f6
moonseo.kim <moonseo.kim@nhn.com>
2021-05-02 17:08:40 +0900

    test inout and argument label

--- Playground/Function.playground/Contents.swift
+++ Playground/Function.playground/Contents.swift
@@ -26,3 +26,20 @@ func addNumbers(numbers: Int...) -> Int {
 
 addNumbers(numbers: 1, 2, 3, 4, 5)
 
+func calculate(multiflyFirstNumber num1: Int, bySecondNumber num2: Int) -> Int {
+    return num1 * num2
+}
+
+calculate(multiflyFirstNumber: 3, bySecondNumber: 4) // argument label test
+
+// inout test
+func addTwoNum(num1: Int, num2: inout Int) -> Int {
+    num2 = 7;
+    return num1 + num2
+}
+
+var test1 = 1
+var test2 = 2
+
+addTwoNum(num1: test1, num2: &test2)
+print(test2)
