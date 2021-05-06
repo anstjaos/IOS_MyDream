9b398af9eb3aac527d65974452da3e08bb1f0b5d
moonseo.kim <moonseo.kim@nhn.com>
2021-05-06 19:29:24 +0900

    add sound effect

--- Flashlight/Flashlight/ViewController.swift
+++ Flashlight/Flashlight/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,15 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var flashImageView: UIImageView!
     
     var isOn = false
+    var soundPlayer: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        prepareSound()
     }
 
+    func prepareSound() {
+        let path = Bundle.main.path(forResource: "switch.wav", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.prepareToPlay()
+        } catch {
+            // couldn't load file :(
+        }
+    }
     
     @IBAction func switchTapped(_ sender: Any) {
         isOn = !isOn
+        soundPlayer?.play()
         flashImageView.image = isOn ? #imageLiteral(resourceName: "onBG") : #imageLiteral(resourceName: "offBG");
         switchButton.setImage(isOn ? #imageLiteral(resourceName: "onSwitch") : #imageLiteral(resourceName: "offSwitch"), for: .normal)
     }
