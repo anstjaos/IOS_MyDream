bfb3c520fea371138bb7f364b98bc5f172bb1f1b
moonseo.kim <moonseo.kim@nhn.com>
2021-05-06 18:56:53 +0900

    create new project & init UI

---
+++ Flashlight/Flashlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by NHN on 2021/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
